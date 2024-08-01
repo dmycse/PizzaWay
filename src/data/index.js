588d21ce7cbce306b563464fcb1578d578571b80
dmycse <dmycse@gmail.com>
2024-08-01 11:03:46 +0300

    Create data file

---
+++ src/data/index.js
@@ -0,0 +1,67 @@
+export const navBar = [
+  {id: 1, title: 'Home', url: '/'},
+  {id: 2, title: 'Menu', url: '/'},
+  {id: 3, title: 'About', url: '/'},
+  {id: 4, title: 'Contact', url: '/'},
+  // {id: 5, title: 'Login', url: '/'},
+];
+
+export const bestSellers = [
+  {
+    id: 1, 
+    title: 'Piccante',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit', 
+    url: '/assets/tomatosalami.png',
+    price: 17
+  },{
+    id: 2, 
+    title: 'Ham',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit', 
+    url: '/assets/tomatomashrooms.png',
+    price: 19
+  },{
+    id: 3, 
+    title: 'Mozzarella', 
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit', 
+    url: '/assets/tomatomozarella.png',
+    price: 18
+  },{
+    id: 4, 
+    title: 'Brocoli', 
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit', 
+    url: '/assets/brocolipizza.png',
+    price: 15
+  },{
+    id: 5, 
+    title: 'Beef', 
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit', 
+    url: '/assets/beefpizza.png',
+    price: 22
+  },{
+    id: 6, 
+    title: 'Cheese', 
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit', 
+    url: '/assets/cheesepizza.png',
+    price: 15
+  }
+];
+
+export const aboutUs = {
+  subHeader: 'our story',
+  mainHeader: 'About us',
+  description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis ex, 
+                distinctio ipsa eius eos deleniti aspernatur id voluptatum impedit nesciunt non? 
+                Aliquid, eaque possimus quisquam rem quod quasi nulla, odit at iure quia dolor ex! Illum, 
+                earum quidem. Laudantium nisi distinctio pariatur reprehenderit ipsa rem cum maiores hic at animi`,
+  extraDescription: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis ex, 
+                distinctio ipsa eius eos deleniti aspernatur id voluptatum impedit nesciunt non? 
+                Aliquid, eaque possimus quisquam rem quod quasi nulla, odit at iure quia dolor ex! Illum, 
+                earum quidem. Laudantium nisi distinctio pariatur reprehenderit ipsa rem cum maiores hic at animi`              
+};
+
+export const contactUs = {
+  subHeader: 'Don\'t hesitate',
+  mainHeader: 'Contact us',
+  phone: '+47 86 54 55 312',
+  email: 'contact@pizzaway.com'
+};
