7fc8f69aa8963eb2d0f6c3da0dd2f8c94127d17e
dmycse <dmycse@gmail.com>
2024-08-01 11:03:20 +0300

    Create contact us section

---
+++ src/components/layout/ContactUs.jsx
@@ -0,0 +1,26 @@
+import { HeaderSection } from "@/components";
+import { contactUs } from "@/data";
+
+export default function ContactUs() {
+
+  let {subHeader, mainHeader, phone, email } = contactUs;
+
+  return (
+    <section className="my-6">
+      <div className="wrapper flex flex-col items-center">
+        <HeaderSection 
+          subHeader={subHeader}
+          mainHeader={mainHeader} 
+        />
+        <div className="mt-4 flex flex-col gap-2 items-center text-3xl text-gray-500">
+          <a href={`tel:${phone.replace(/\s/g,'')}`}>
+          {phone}
+          </a>
+          <a href={`mailto:${email}`}>
+            {email}
+          </a>
+        </div>
+      </div>
+    </section>
+  )
+}
