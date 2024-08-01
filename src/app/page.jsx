d11cd1916fe90ef6dd3c0bffa4aed18a0fd1cf69
dmycse <dmycse@gmail.com>
2024-08-01 11:05:08 +0300

    Create app home page

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import { Hero,  HomeMenu, AboutUs, ContactUs } from "@/components";
+
+
+export default function Home() {
+  return (
+    <main className="flex-1">
+      <Hero />
+      <HomeMenu />
+      <AboutUs />
+      <ContactUs />
+    </main>
+  );
+}
