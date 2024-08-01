ab9a10f7f05a6e073556e9dbb39ba232c8058fbc
dmycse <dmycse@gmail.com>
2024-08-01 11:04:43 +0300

    Create app layout

---
+++ src/app/layout.jsx
@@ -0,0 +1,29 @@
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+import { Header } from "@/components";
+import { Footer } from "@/components";
+
+const roboto = Roboto({ 
+  subsets: ["latin"],
+  weight: ['400', '500', '700'] 
+});
+
+export const metadata = {
+  title: "PizzaWay",
+  description: "Greate Pizzas for takeaway and delivery",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>
+        <div className="h-screen flex flex-col">
+          <Header />
+          {children}
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
