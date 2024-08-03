a283089e09edce041b1799b9946a1ff83cc8e581
dmycse <dmycse@gmail.com>
2024-08-03 10:53:04 +0300

    Create footer section

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <footer className='mt-16 p-8 border-t text-center text-gray-600'>
+      &copy;DMY PizzaWay - 2024 All rights reserved
+    </footer>
+  )
+}
