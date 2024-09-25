2bbdcbee4bb9f2f7e8cc67c39f8363f9433f6c49
dmycse <dmycse@gmail.com>
2024-09-25 15:54:52 +0300

    Feat(shared): create container component

---
+++ components/shared/container.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import { cn } from '@/lib/utils';
+
+
+type Props = {
+    className?: string
+    children: ReactNode
+};
+
+
+export let Container = ({className, children}: Props) => {
+
+  return (
+    <div className={ cn('container px-4 max-sm:px-2 mx-auto', className) }>
+      {children}
+    </div>
+  );
+};
