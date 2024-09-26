c10dda1413b2374d71582048550fa0674a33f84d
dmycse <dmycse@gmail.com>
2024-09-26 17:36:19 +0300

    Create mock data for development

---
+++ data.ts
@@ -0,0 +1,18 @@
+export let categories = ['Pizzas', 'Combo', 'Starters', 'Coctails', 'Drinks', 'Desserts'];
+
+export let ingradients = [
+  {label: 'Cheese sauce', value: '1'},
+  {label: 'Mozzarella', value: '2'},
+  {label: 'Garlic', value: '3'},
+  {label: 'Pikles', value: '4'},
+  {label: 'Red onion', value: '5'},
+  {label: 'Tomatos', value: '6'},
+
+  {label: 'Cheese sauce2', value: '7'},
+  {label: 'Mozzarella2', value: '8'},
+  {label: 'Garlic2', value: '9'},
+  {label: 'Pikles2', value: '10'},
+  {label: 'Red onion2', value: '11'},
+  {label: 'Tomatos2', value: '12'},
+
+]
