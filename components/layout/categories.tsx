e4538e12765e3e131d2ba386ce77b409e713c6c0
dmycse <dmycse@gmail.com>
2024-09-26 12:41:26 +0300

    Refactor(layout): edit link in categories component

--- components/layout/categories.tsx
+++ components/layout/categories.tsx
@@ -13,13 +13,13 @@ const activeIndex = 0;
 export let Categories = ({className}: Props) => {
 
   return (
-    <div className={ cn('my-3 inline-flex gap-2', className) }>
+    <div className={ cn('inline-flex gap-2', className) }>
       {
         categories.map((category, index) => (
           <Button variant='outline' key={category} asChild className={cn('px-5 h-11 text-gray-500 shadow-md shadow-dray-200 rounded-md border-none hover:bg-white hover:text-primary',
             activeIndex === index && 'bg-white text-primary' 
           )}>
-            <Link href={`/categories/${category}`}>
+            <Link href={`/categories/${category.toLowerCase()}`}>
               {category}
             </Link>
           </Button>
