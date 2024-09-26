1b3eac090c242ac7db9558d681dce57a712a2a1b
dmycse <dmycse@gmail.com>
2024-09-26 17:37:56 +0300

    Ffeat(shared): create range-slider component for filter products by price

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,3 +1,5 @@
 export { Container } from '@/components/shared/container';
 export { Title } from '@/components/shared/title';
-export { FilterCheckbox } from '@/components/shared/filter-checkbox'
+export { FilterCheckbox } from '@/components/shared/filter-checkbox';
+export { FilterCheckboxGroup } from '@/components/shared/filter-checkbox-group';
+export { RangeSlider } from '@/components/shared/range-slider';
