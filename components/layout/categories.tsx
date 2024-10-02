e71dbdd2ecb45ece14af0245c05b421ead5e2cfc
dmycse <dmycse@gmail.com>
2024-10-02 14:28:47 +0300

    Refactor(layout): refactor categories component

--- components/layout/categories.tsx
+++ components/layout/categories.tsx
@@ -20,9 +20,9 @@ export let Categories = ({className}: Props) => {
   return (
     <div className={ cn('inline-flex gap-2', className) }>
       {
-        categories.map(({name}) => (
-          <Button variant='outline' key={id} asChild className={cn('px-5 h-11 text-gray-500 shadow-md shadow-dray-200 rounded-md border-none hover:bg-white hover:text-primary',
-            categoryActiveId === id && 'bg-white text-primary' 
+        categories.map(({name}, index) => (
+          <Button variant='outline' key={index} asChild className={cn('px-5 h-11 text-gray-500 shadow-md shadow-dray-200 rounded-md border-none hover:bg-white hover:text-primary',
+            categoryActiveId === (index + 1) && 'bg-white text-primary' 
           )}>
             <Link href={`/#${name}`}>
               {name}
