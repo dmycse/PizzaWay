a4607ac96b98339ce06ec632f40a55dbb2ace37c
dmycse <dmycse@gmail.com>
2024-10-17 11:03:24 +0300

    Feat(hooks): add users filter choice for storing

--- hooks/useFilterIngredients.ts
+++ hooks/useFilterIngredients.ts
@@ -1,11 +1,29 @@
 import { useEffect, useState } from "react";
+import { useSet } from "react-use";
+
 import { Ingredient } from "@prisma/client";
 import { getAllIngredients } from "@/utils/ingredients";
 
+type UseFilterIngredientsReturnedProps = {
+  ingredients: Ingredient[];
+  selectedIngredients: Set<string>;
+  selectedPizzaSize: Set<string>;
+  selectedPizzaCrust: Set<string>;
+  setSelectedIngredient: (value: string) => void;
+  setSelectedPizzaSize: (value: string) => void;
+  setSelectedPizzaCrust: (value: string) => void;
+}
 
-export let useFilterIngredients = (): Ingredient[] => {
+export let useFilterIngredients = (): UseFilterIngredientsReturnedProps => {
 
   let [ingredients, setIngredients] = useState<Ingredient[]>([]);
+  // store selected ingredients
+  let [selectedIngredients, { toggle: toggleIngredient }] = useSet(new Set<string>([]));
+  // store selected pizza size
+  let [selectedPizzaSize, { toggle: togglePizzaSize }] = useSet(new Set<string>([]));
+  // store selected pizza crust
+  let [selectedPizzaCrust, { toggle: togglePizzaCrust }] = useSet(new Set<string>([]));
+  
 
   useEffect(() => {
     getAllIngredients()
@@ -13,5 +31,13 @@ export let useFilterIngredients = (): Ingredient[] => {
       .catch(error => console.warn(error));
   }, []);
 
-  return ingredients || [];
+  return {
+    ingredients, 
+    selectedIngredients,
+    selectedPizzaSize,
+    selectedPizzaCrust, 
+    setSelectedIngredient: toggleIngredient,
+    setSelectedPizzaSize: togglePizzaSize,
+    setSelectedPizzaCrust: togglePizzaCrust
+  };
 }
