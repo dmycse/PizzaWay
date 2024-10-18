3b4f5a5f7a696623f590e5a0bcb34fd33614eb24
dmycse <dmycse@gmail.com>
2024-10-18 20:36:22 +0300

    Feat(layout): add logic of categories  display

--- components/layout/categories.tsx
+++ components/layout/categories.tsx
@@ -1,28 +1,27 @@
 'use client';
 
 import Link from 'next/link';
-import { cn } from '@/lib/utils';
 import { Button } from '../ui';
-import { categories } from '@/prisma/constants';
 import { useCategoryStore } from '@/store/category';
+import { Category } from '@prisma/client';
+import { cn } from '@/lib/utils';
 
-type Props = {
-    className?: string
+type CategoriesProps = {
+  categories: Category[],
+  className?: string
 };
 
-const activeIndex = 0;
 
+export let Categories = ({ categories, className}: CategoriesProps) => {
 
-export let Categories = ({className}: Props) => {
-
-  let categoryActiveId = useCategoryStore(state => state.activeId);
-
+  let activeCategoryId = useCategoryStore(state => state.activeCategoryId);
+  
   return (
     <div className={ cn('inline-flex gap-2', className) }>
       {
-        categories.map(({name}, index) => (
+        categories.map(({id, name}, index) => (
           <Button variant='outline' key={index} asChild className={cn('px-5 h-11 text-gray-500 shadow-md shadow-dray-200 rounded-md border-none hover:bg-white hover:text-primary',
-            categoryActiveId === (index + 1) && 'bg-white text-primary' 
+            activeCategoryId === id && 'bg-white text-primary' 
           )}>
             <Link href={`/#${name}`}>
               {name}
--- components/layout/topbar.tsx
+++ components/layout/topbar.tsx
@@ -1,20 +1,21 @@
 import { Container } from '@/components/shared';
 import { Categories,  SortPopup} from '@/components/layout';
+import { Category } from '@prisma/client';
 
 import { cn } from '@/lib/utils';
 
 type TopBarProps = {
-    className?: string
-
+    categories: Category[];
+    className?: string;
 };
 
 
-export let TopBar = ({className}: TopBarProps) => {
+export let TopBar = ({ categories, className}: TopBarProps) => {
 
   return (
     <div className={ cn('py-5 sticky top-0 z-10 bg-white shadow-lg', className) }>
       <Container className='flex items-center justify-between'>
-        <Categories />
+        <Categories categories={categories} />
         <SortPopup />
       </Container>
     </div>
