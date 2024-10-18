05a80d0a6eb02a0c32dda60d106321baaf6d346a
dmycse <dmycse@gmail.com>
2024-10-18 20:38:46 +0300

    Refactor(app): refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,124 +1,56 @@
-import { Container, ProductCard, ProductsGroup, Title } from "@/components/shared";
+import { Container, ProductsGroup, Title } from "@/components/shared";
 import { TopBar, Filters } from "@/components/layout";
+import { prisma } from "@/prisma/prisma-client";
 
 
 
-export default function Home() {
+export default async function Home() {
+
+  let categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          options: true
+        }
+      }
+    }
+  });
+  console.log(categories[0].products[0])
   return (
     <>
       <Container className='mt-8'>
-        <Title text='Pizzas & more' size='lg' className="font-extrabold"/>
+        <Title text='Pizzas & More' size='lg' className="font-extrabold"/>
       </Container>
 
-      <TopBar />
+      <TopBar categories={categories.filter(category => category.products.length > 0)} />
 
       <Container className="pb-14">
-        <div className="flex gap-16">
+        <div className="flex gap-20">
 
-          <div className="w=[250px]">
+          <div className="w-[250px]">
             <Filters />
           </div>
 
-          <div className="mt-10 flex-1">
+          <div className="mt-8 flex-1">
             <div className="flex flex-col gap-16">
-              <ProductsGroup title="Pizzas" items={[
-                {
-                  id: 1, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-                {
-                  id: 2, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-                {
-                  id: 3, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-                {
-                  id: 4, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-                {
-                  id: 5, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-                {
-                  id: 6, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-              ]} categoryId={1} />
-              <ProductsGroup title="Branches" items={[
-                {
-                  id: 1, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-                {
-                  id: 2, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-                {
-                  id: 3, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-                {
-                  id: 4, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-                {
-                  id: 5, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-                {
-                  id: 6, 
-                  name: "Carbonara", 
-                  image_url: "/images/products/beefpizza.webp",
-                  price: 10,
-                  items: [{price: 10}],
-                },
-              ]} categoryId={1} />
+              {
+                categories.map(category => (
+                  category.products.length > 0 && (
+                   <ProductsGroup 
+                      key={category.id} 
+                      title={category.name} 
+                      categoryId={category.id}
+                      items={category.products}
+                  />
+                  )
+                ))
+              }
             </div>
           </div>
           
         </div>
       </Container>
-
-
-    
-    
     </>
-    
   );
 }
