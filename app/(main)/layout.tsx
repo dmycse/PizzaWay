0601970107d03627ed73dc5d840925a68ff37d0a
dmycse <dmycse@gmail.com>
2024-10-24 12:44:34 +0300

    Feat(app layout): add parallel route to MainLayout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,18 +4,21 @@ import { Header } from "@/components/layout";
 
 
 export const metadata: Metadata = {
-  title: "Pizzas | PizzaWay",
+  title: "Pizzas&More | PizzaWay",
   description: "Delicious Pizzas for takeaway and delivery.",
 };
 
 export default function MainLayout(
-  { children }: Readonly<{children: ReactNode}>
-  ) {
+  { children, modal }: Readonly<{
+    children: ReactNode,
+    modal: ReactNode
+  }>) {
   return (
     <>
       <Header />
       <main className="min-h-screen">
         {children}
+        {modal}
       </main>
     </>
   );
