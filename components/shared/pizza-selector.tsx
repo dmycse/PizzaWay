1c82f91356321ca27af07926b12a851ec3dfcb3c
dmycse <dmycse@gmail.com>
2024-10-24 13:14:58 +0300

    Feat(components): create component for pizza size or type choosing

---
+++ components/shared/pizza-selector.tsx
@@ -0,0 +1,39 @@
+
+import { cn } from '@/lib/utils';
+
+
+export type PizzaVariant = {
+  name: string;
+  value: string;
+  disabled?: boolean;
+};
+
+type PizzaSelectorProps = {
+  items: PizzaVariant[];
+  value?: PizzaVariant['value'];
+  onClick?: (value: PizzaVariant['value']) => void;
+  className?: string;
+};
+
+
+export let PizzaSelector = ({ items, value, onClick, className }: PizzaSelectorProps) => {
+
+  return (
+    <div className={cn(className, 'p-1 flex justify-between bg-[#F3F3F7] rounded-3xl select-none')}>
+      {items.map(item => (
+        <button
+          key={item.name}
+          onClick={() => onClick?.(item.value)}
+          className={cn(
+            'px-5 h-[30px] flex-1 flex justify-center items-center text-sm cursor-pointer rounded-3xl transition-all duration-400',
+            {
+              'bg-white shadow': item.value === value,
+              'text-gray-500 opacity-50 pointer-events-none': item.disabled,
+            },
+          )}>
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
+};
