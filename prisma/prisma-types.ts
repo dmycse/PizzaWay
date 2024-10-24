effbe59bfc413b4321d105e52ff5432bf33eb66c
dmycse <dmycse@gmail.com>
2024-10-24 12:48:35 +0300

    Feat(prisma): create additional types

---
+++ prisma/prisma-types.ts
@@ -0,0 +1,24 @@
+import { Product, ProductOption, Ingredient} from '@prisma/client';
+
+export type ProductWithRelations = Product & {
+  options: ProductOption[];
+  ingredients: Ingredient[];
+};
+
+
+export const mapPizzaSizes = {
+  25: 'Small',
+  30: 'Middle',
+  35: 'Big',
+} as const;
+
+export const mapPizzaType = {
+  1: 'thin',
+  2: 'classic',
+} as const;
+
+
+export type PizzaSize = keyof typeof mapPizzaSizes;
+
+export type PizzaType = keyof typeof mapPizzaType;
+
