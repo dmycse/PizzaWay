c626632dd0e729c906df15b1df49d4e0f04aa268
dmycse <dmycse@gmail.com>
2024-10-26 10:30:24 +0300

    Feat(components): create pizza-variant component

---
+++ components/shared/pizzas/index.tsx
@@ -0,0 +1,4 @@
+export { PizzaImage } from './pizza-image';
+export { PizzaSelector } from './pizza-selector';
+export { PizzaVariant } from './pizza-variant';
+export { ChoosePizza } from './choose-pizza';
