98d8189f6f6f7bc83872c9e720005d44fa7c41e5
dmycse <dmycse@gmail.com>
2024-10-28 10:09:00 +0200

    Feat(lib): create getTotalPizzaPrice function

---
+++ lib/getTotalPizzaPrice.ts
@@ -0,0 +1,34 @@
+import { Ingredient, ProductOption } from '@prisma/client';
+import { type PizzaSize, type PizzaType } from '@/prisma/prisma-types';
+
+/**
+ * Function: pizza total price calculation. 
+ * 
+ * Used in: ChoosePizza
+ * @param selectedSize - size of selected pizza
+ * @param selectedType - cruct type of selected pizza
+ * @param options - list of pizza options
+ * @param ingredients - list of pizza ingredients
+ * @param selectedIngredients - selected ingredients for pizza sumplements
+ * @returns number total pizza price
+ */
+export const getTotalPizzaPrice = (
+  selectedType: PizzaType,
+  selectedSize: PizzaSize,
+  options: ProductOption[],
+  ingredients: Ingredient[],
+  selectedIngredients: Set<number>,
+) => {
+
+  let pizzaPriceByOptions = 
+    options.find(o => o.pizzaSize === selectedSize && o.pizzaType === selectedType)?.price ?? 0;
+
+  let pizzaPriceByIngredients = ingredients.reduce((sum, ingredient) => {
+    if (selectedIngredients.has(ingredient.id)) {
+      return sum += ingredient.price;
+    }
+    return sum;
+  }, 0); 
+
+  return pizzaPriceByOptions + pizzaPriceByIngredients;
+};
