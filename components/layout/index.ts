d8266245e98aad2847e26a6dfe14b5dd1fc68f84
dmycse <dmycse@gmail.com>
2024-10-28 21:08:13 +0200

    Refactor(components): change folder of title component

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -3,3 +3,4 @@ export { Header } from '@/components/layout/header';
 export { Categories } from '@/components/layout/categories';
 export { SortPopup } from '@/components/layout/sort_popup';
 export { TopBar } from '@/components/layout/topbar';
+export { Title } from '@/components/layout/title';
