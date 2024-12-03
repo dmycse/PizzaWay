672c836856d5c679eeaecd3a4500d85af94f0c66
dmycse <dmycse@gmail.com>
2024-12-03 22:12:16 +0200

    Refactor(app): edit app/products/[id] page

--- app/(main)/products/[id]/page.tsx
+++ app/(main)/products/[id]/page.tsx
@@ -1,13 +1,14 @@
+import { notFound } from 'next/navigation';
 import { Container } from '@/components/layout';
-import { PizzaVariant } from '@/components/shared';
+// import { PizzaVariant } from '@/components/shared';
 import { prisma } from '@/prisma/prisma-client';
+import { ProductSelection } from '@/components/shared';
 
 type ProductPageProps = {
     params: {
       id: string
     }
 };
-
 
 /**
  * Component:pPage that displays ditaled information of product by id.
@@ -16,32 +17,38 @@ type ProductPageProps = {
  * that displays the product. If the product is not found, it returns a message
  * indicating that the product was not found.
  * 
- * @param {Object} props
- * @prop {Object} params
- * @prop {string} params.id - id of the product to display
+ * @param {ProductPageProps} props
+ * @prop  {Object} params
+ * @prop  {string} params.id - id of the product to display
  * 
  * @returns {JSX.Element} The component that displays the product
  */
 export default async function ProductPage({params: { id }}: ProductPageProps) {
 
-  let product = await prisma.product.findFirst({
-    where: {
-      id: Number(id),
-    }
+  const product = await prisma.product.findFirst({
+    where: { id: Number(id) },
+    include: {
+      ingredients: true,
+      category: {
+        include: {
+          products: {
+            include: {
+              options: true,
+            },
+          },
+        },
+      },
+      options: true,
+    },
   });
 
   if (!product) {
-    return (
-      <div>
-        Product not found
-      </div>
-    );
+    return notFound();
   }
 
   return (
     <Container className='my-10 flex flex-col'>
-      pizza
-      {/* <PizzaVariant {...product} className='' /> */}
+      <ProductSelection product={product} />
     </Container>
   );
 };
