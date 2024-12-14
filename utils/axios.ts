5a042c41f333b006d4b3413fd5174b6175011d3b
dmycse <dmycse@gmail.com>
2024-12-14 13:01:37 +0200

    Refactor(axios): disable timeout prop

--- utils/axios.ts
+++ utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let requestConfig = {
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 3000
+  // timeout: 3000
 };
 
 export const axiosInstance = axios.create(requestConfig);
