de64a2c8c4c84dc816a9bd8addf85a0dadba3d3f
dmycse <dmycse@gmail.com>
2024-12-26 23:41:49 +0200

    Refactor(prisma->data): create products list data

---
+++ prisma/products_data/products.ts
@@ -0,0 +1,104 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Ham&mushrooms Omelette',
+    imageUrl: '/images/products/breakfast/omelette_ham.webp',
+    categoryId: 2,
+  },
+  {
+    id: 2,
+    name: 'Pepperoni Omelette',
+    imageUrl: '/images/products/breakfast/omelette_pepperoni.webp',
+    categoryId: 2,
+  },
+  {
+    id: 3,
+    name: 'Bacon Omelette',
+    imageUrl: '/images/products/breakfast/omelette_bacon.webp',
+    categoryId: 2,
+  },
+  {
+    id: 4,
+    name: 'Cheese Omelette',
+    imageUrl: '/images/products/breakfast/omelette_cheese.webp',
+    categoryId: 2,
+  },
+  {
+    id: 5,
+    name: 'Mashrooms Starter',
+    imageUrl: '/images/products/breakfast/starter_mushrooms.webp',
+    categoryId: 2,
+  },
+  {
+    id: 6,
+    name: 'Cheese Sarter',
+    imageUrl: '/images/products/breakfast/starter_cheese.webp',
+    categoryId: 2,
+  },
+  {
+    id: 7,
+    name: 'Cheese Pancakes',
+    imageUrl: '/images/products/breakfast/cheese_pancakes.webp',
+    categoryId: 2,
+  },
+  {
+    id: 8,
+    name: 'Cheese Pancakes & Cream',
+    imageUrl: '/images/products/breakfast/cheese_pancakes_cream.webp',
+    categoryId: 2,
+  },
+  {
+    id: 9,
+    name: 'Cheese Pancakes & Jam',
+    imageUrl: '/images/products/breakfast/cheese_pancakes_jam.webp',
+    categoryId: 2,
+  },
+  {
+    id: 10,
+    name: 'Latte',
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
+    categoryId: 3,
+  },
+  {
+    id: 11,
+    name: 'Ham and cheese sandwich',
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796FF0059B799A17F57A9E64C725.webp',
+    categoryId: 3,
+  },
+  {
+    id: 12,
+    name: 'Chicken Nuggets',
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D618B5C7EC29350069AE9532C6E.webp',
+    categoryId: 3,
+  },
+  {
+    id: 13,
+    name: 'Baked potatoes with sauce 🌱',
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EED646A9CD324C962C6BEA78124F19.webp',
+    categoryId: 3,
+  },
+  {
+    id: 14,
+    name: 'Dodster',
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796F96D11392A2F6DD73599921B9.webp',
+    categoryId: 3,
+  },
+  {
+    id: 15,
+    name: 'Spicy Dodster 🌶️',
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796FD3B594068F7A752DF8161D04.webp',
+    categoryId: 3,
+  },
+  {
+    id: 16,
+    name: 'Banana Milkshake',
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EEE20B8772A72A9B60CFB20012C185.webp',
+    categoryId: 4,
+  },
+  {
+    id: 17,
+    name: 'Caramel Apple Milkshake',
+    imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE79702E2A22E693D96133906FB1B8.webp',
+    categoryId: 4,
+  }
+].map(({ name, imageUrl, categoryId }) => ({ name, imageUrl, categoryId }));
