69032e0fcd96d711c3885c99e78a08179154652f
dmycse <dmycse@gmail.com>
2025-01-02 15:27:04 +0200

    Refactor(components providers): edit JSDoc in providers component

--- components/providers/providers.tsx
+++ components/providers/providers.tsx
@@ -5,6 +5,15 @@ import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
 import NextTopLoader from 'nextjs-toploader';
 
+/**
+ * Providers wraps the entire app with all the necessary providers.
+ *
+ * SessionProvider is used for authentication.
+ * Toaster is used for toast notifications.
+ * NextTopLoader is used to show a loading bar on the top of the page.
+ *
+ * Used in RootLayout -> app/layout.tsx
+ */
 export const Providers = ({ children }: {children: ReactNode}) => {
   return (
     <>
