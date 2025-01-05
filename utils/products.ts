69de1267943606fbe4b56d67da102165db8193dd
dmycse <dmycse@gmail.com>
2025-01-05 19:12:14 +0200

    Refactor(utils): add JSDco to products util

--- utils/products.ts
+++ utils/products.ts
@@ -2,6 +2,10 @@ import { Product } from "@prisma/client";
 import { axiosInstance } from "@/utils/axios";
 import { ApiRoutes } from "@/utils/api-constants";
 
+/**
+ * Asynchronously searches for products based on the provided query string.
+ */
+
 export const searchProducts = async (query: string): Promise<Product[]> => {
   let { data } = await axiosInstance.get<Product[]>(`${ApiRoutes.SEARCH_PRODUCTS}?q=${query}`);
   
